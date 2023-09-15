ae469b6b48d85f8b6065e8aa59ae12540888cdb0
Wren <OtherwiseJunk@gmail.com>
2023-09-15 16:26:14 -0400

    add pinia

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
-import { createApp } from 'vue';
-import { store } from './store';
-import { pluralize } from './pluralize';
-import { humanize } from './humanize';
-import App from './App.vue';
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import { store } from "./store";
+import { pluralize } from "./pluralize";
+import { humanize } from "./humanize";
+import App from "./App.vue";
 
 const app = createApp(App);
+const pinia = createPinia();
+app.use(pinia);
 app.use(store);
 
 // Apply pluralize as a global filter.
@@ -12,4 +15,4 @@ app.config.globalProperties.$filters = {};
 app.config.globalProperties.$filters.pluralize = pluralize;
 app.config.globalProperties.$filters.humanize = humanize;
 
-app.mount('#app');
+app.mount("#app");
