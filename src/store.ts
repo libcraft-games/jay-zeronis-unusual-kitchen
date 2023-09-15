3a417e599ed75ce3ec1b62277baf927a42a4f8e7
Wren <OtherwiseJunk@gmail.com>
2023-09-15 16:29:38 -0400

    rename state to gamestate

--- src/store.ts
+++ src/store.ts
@@ -1,19 +1,19 @@
-import { createStore, Store } from 'vuex';
-import createPersistedState from 'vuex-persistedstate';
-import { cloneDeep } from 'lodash';
+import { createStore, Store } from "vuex";
+import createPersistedState from "vuex-persistedstate";
+import { cloneDeep } from "lodash";
 
 // TODO: Figure out how to import this from a TypeScript file not in the
 //       public directory.
-const worker = new Worker('./worker.js');
+const worker = new Worker("./worker.js");
 
-type Purchasable = 'autobrewer';
-type Upgradable = 'autobrewer';
+type Purchasable = "autobrewer";
+type Upgradable = "autobrewer";
 
 // How often ticks happen, in milliseconds.
 export const TICK_RATE = 200;
 export const TICKS_PER_SECOND = 1000 / TICK_RATE;
 
-export type State = {
+export type GameState = {
   lastSaveAt: number | null;
   lastNotableTickAt: number | null;
   tick: number;
@@ -31,7 +31,7 @@ export type State = {
       count: number;
       price: number;
       increaseRate: number;
-    }
+    };
   };
   upgrades: {
     [key in Upgradable]: {
@@ -40,7 +40,7 @@ export type State = {
       outputMultiplier: number;
       costMultiplier: number;
       nextUpgradeCost: number;
-    }
+    };
   };
   debugMode: boolean;
 };
@@ -49,17 +49,17 @@ const BASE_VALUES = {
   purchases: {
     autobrewer: {
       price: 10,
-      increaseRate: 1.01
-    }
+      increaseRate: 1.01,
+    },
   },
   upgrades: {
     autobrewer: {
       outputMultiplier: 2.0,
       costMultiplier: 5,
-      nextUpgradeCost: 100
-    }
+      nextUpgradeCost: 100,
+    },
   },
-  demand: 100
+  demand: 100,
 };
 
 // The state of the game when the game starts or is manually reset.
@@ -70,19 +70,19 @@ const getDefaultGameState = () => {
     tick: 0,
     money: 0,
     cupsOfTea: 0,
-    teaPrice: 2.00,
+    teaPrice: 2.0,
     teaPerTick: 0,
     rawDemand: BASE_VALUES.demand,
     demandBonuses: {
       level: 1,
-      tastiness: 1
+      tastiness: 1,
     },
     purchases: {
       autobrewer: {
         count: 0,
         price: BASE_VALUES.purchases.autobrewer.price,
-        increaseRate: BASE_VALUES.purchases.autobrewer.increaseRate
-      }
+        increaseRate: BASE_VALUES.purchases.autobrewer.increaseRate,
+      },
     },
     upgrades: {
       autobrewer: {
@@ -90,23 +90,23 @@ const getDefaultGameState = () => {
         currentOutputMultiplier: 1.0,
         outputMultiplier: BASE_VALUES.upgrades.autobrewer.outputMultiplier,
         costMultiplier: BASE_VALUES.upgrades.autobrewer.costMultiplier,
-        nextUpgradeCost: BASE_VALUES.upgrades.autobrewer.nextUpgradeCost
-      }
+        nextUpgradeCost: BASE_VALUES.upgrades.autobrewer.nextUpgradeCost,
+      },
     },
-    debugMode: false
+    debugMode: false,
   };
 };
 
-export const store: Store<State> = createStore({
+export const store: Store<GameState> = createStore({
   strict: !import.meta.env.PROD,
   plugins: [
     createPersistedState({
-      key: 'teaShopSave',
+      key: "teaShopSave",
       // Only trigger saving when the triggerSave method is called.
       filter: (mutation) => {
-        return mutation.type === 'triggerSave';
-      }
-    })
+        return mutation.type === "triggerSave";
+      },
+    }),
   ],
   state() {
     return getDefaultGameState();
@@ -136,37 +136,48 @@ export const store: Store<State> = createStore({
       } else {
         state.teaPrice = 0.01;
         if (state.debugMode) {
-          console.log('Unable to decrease price any further.');
+          console.log("Unable to decrease price any further.");
         }
       }
     },
     recalculateRawDemand(state) {
-      state.rawDemand = BASE_VALUES.demand * state.demandBonuses.level * state.demandBonuses.tastiness;
+      state.rawDemand =
+        BASE_VALUES.demand *
+        state.demandBonuses.level *
+        state.demandBonuses.tastiness;
     },
     buyAutobrewer(state, amount = 1) {
       state.purchases.autobrewer.count += amount;
     },
-    increasePurchasablePrice(state, payload: { purchasable: Purchasable, amount: number | null }) {
+    increasePurchasablePrice(
+      state,
+      payload: { purchasable: Purchasable; amount: number | null }
+    ) {
       if (payload.amount === null) {
         payload.amount = 1;
       }
-      state.purchases[payload.purchasable].price *= Math.pow(state.purchases[payload.purchasable].increaseRate, payload.amount);
+      state.purchases[payload.purchasable].price *= Math.pow(
+        state.purchases[payload.purchasable].increaseRate,
+        payload.amount
+      );
     },
     setLastNotableTickAt(state, payload: { datetime: number }) {
       state.lastNotableTickAt = payload.datetime;
     },
     upgradeUpgradable(state, payload: { upgradable: Upgradable }) {
       state.upgrades[payload.upgradable].level += 1;
-      state.upgrades[payload.upgradable].currentOutputMultiplier *= state.upgrades[payload.upgradable].outputMultiplier;
-      state.upgrades[payload.upgradable].nextUpgradeCost *= state.upgrades[payload.upgradable].costMultiplier;
+      state.upgrades[payload.upgradable].currentOutputMultiplier *=
+        state.upgrades[payload.upgradable].outputMultiplier;
+      state.upgrades[payload.upgradable].nextUpgradeCost *=
+        state.upgrades[payload.upgradable].costMultiplier;
     },
     // This mutation automatically triggers the persisted state plugin to activate.
     triggerSave(state) {
       state.lastSaveAt = Date.now();
     },
     hardReset(_state) {
-      if (localStorage.getItem('teaShopSave') !== null) {
-        localStorage.removeItem('teaShopSave');
+      if (localStorage.getItem("teaShopSave") !== null) {
+        localStorage.removeItem("teaShopSave");
       }
       store.replaceState(getDefaultGameState());
     },
@@ -174,20 +185,27 @@ export const store: Store<State> = createStore({
       state.debugMode = !state.debugMode;
     },
     recalculateTeaPerTick(state) {
-      state.teaPerTick = (state.purchases.autobrewer.count * state.upgrades.autobrewer.currentOutputMultiplier) / TICKS_PER_SECOND;
-    }
+      state.teaPerTick =
+        (state.purchases.autobrewer.count *
+          state.upgrades.autobrewer.currentOutputMultiplier) /
+        TICKS_PER_SECOND;
+    },
   },
   actions: {
-    // Have the worker startup in the background and begin ticking. 
+    // Have the worker startup in the background and begin ticking.
     async startup(_context) {
-      worker.postMessage({ name: 'startup', tick_rate: TICK_RATE });
+      worker.postMessage({ name: "startup", tick_rate: TICK_RATE });
     },
     // Handle ticking logic asyncronously from the worker.
     async tick(context) {
-      worker.postMessage({ name: 'tick', state: cloneDeep(context.state), ticks_per_second: TICKS_PER_SECOND });
+      worker.postMessage({
+        name: "tick",
+        state: cloneDeep(context.state),
+        ticks_per_second: TICKS_PER_SECOND,
+      });
     },
     brewTea(context) {
-      context.commit('brewTea');
+      context.commit("brewTea");
     },
     sellTea(context, { amount }) {
       if (context.state.debugMode) {
@@ -195,38 +213,51 @@ export const store: Store<State> = createStore({
       }
       if (amount < 0) {
         if (context.state.debugMode) {
-          console.log('Cannot sell negative amounts of tea.');
+          console.log("Cannot sell negative amounts of tea.");
         }
         amount = 0;
       }
-      context.commit('consumeTea', amount);
-      context.commit('earnMoney', amount * context.state.teaPrice);
+      context.commit("consumeTea", amount);
+      context.commit("earnMoney", amount * context.state.teaPrice);
     },
     upgradeUpgradable(context, { upgradable }: { upgradable: Upgradable }) {
-      context.commit('consumeTea', context.state.upgrades[upgradable].nextUpgradeCost);
-      context.commit('upgradeUpgradable', { upgradable: upgradable });
-      context.commit('recalculateTeaPerTick');
+      context.commit(
+        "consumeTea",
+        context.state.upgrades[upgradable].nextUpgradeCost
+      );
+      context.commit("upgradeUpgradable", { upgradable: upgradable });
+      context.commit("recalculateTeaPerTick");
     },
     buyAutobrewer(context, { amount }) {
       let increaseRate = context.state.purchases.autobrewer.increaseRate;
-      let price = context.state.purchases.autobrewer.price * (1 - Math.pow(increaseRate, amount)) / (1 - increaseRate);
-      context.commit('consumeTea', price);
-      context.commit('increasePurchasablePrice', { purchasable: 'autobrewer', amount: amount });
-      context.commit('buyAutobrewer', amount);
-      context.commit('recalculateTeaPerTick');
+      let price =
+        (context.state.purchases.autobrewer.price *
+          (1 - Math.pow(increaseRate, amount))) /
+        (1 - increaseRate);
+      context.commit("consumeTea", price);
+      context.commit("increasePurchasablePrice", {
+        purchasable: "autobrewer",
+        amount: amount,
+      });
+      context.commit("buyAutobrewer", amount);
+      context.commit("recalculateTeaPerTick");
     },
     // Brew based on the number of autobrewers we have, divided by the number of ticks that occur per second.
     autobrew(context) {
-      context.commit('brewTea', context.state.teaPerTick);
-    }
-  }
+      context.commit("brewTea", context.state.teaPerTick);
+    },
+  },
 });
 
 // Handle incoming messages from the worker as mutations and actions.
 worker.onmessage = (e) => {
-  if (e.data.type === 'mutation') {
-    typeof e.data.payload === 'undefined' ? store.commit(e.data.method) : store.commit(e.data.method, e.data.payload);
-  } else if (e.data.type === 'action') {
-    typeof e.data.payload === 'undefined' ? store.dispatch(e.data.method) : store.dispatch(e.data.method, e.data.payload);
+  if (e.data.type === "mutation") {
+    typeof e.data.payload === "undefined"
+      ? store.commit(e.data.method)
+      : store.commit(e.data.method, e.data.payload);
+  } else if (e.data.type === "action") {
+    typeof e.data.payload === "undefined"
+      ? store.dispatch(e.data.method)
+      : store.dispatch(e.data.method, e.data.payload);
   }
 };
