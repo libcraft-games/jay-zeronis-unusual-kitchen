a27fcbcab0f83c22af32009d6480136992a178d8
Wren <otherwisejunk@gmail.com>
2023-09-16 12:34:55 -0400

    modify worker to loadState and use case

--- public/worker.js
+++ public/worker.js
@@ -1,14 +1,17 @@
 self.onmessage = (e) => {
-  if (e.data.name === "startup") {
-    const TICKS_PER_SECOND = e.data.TICKS_PER_SECOND;
-    const TICK_RATE = e.data.TICK_RATE;
+  switch (e.data.name) {
+    case "startup":
+      const TICKS_PER_SECOND = e.data.TICKS_PER_SECOND;
+      const TICK_RATE = e.data.TICK_RATE;
+      self.postMessage({ name: "loadState" });
 
-    // The heartbeat of the game baybee
-    setInterval(() => {
-      self.postMessage({
-        name: "tick",
-        ticks_per_second: TICKS_PER_SECOND,
-      });
-    }, TICK_RATE);
+      // The heartbeat of the game baybee
+      setInterval(() => {
+        self.postMessage({
+          name: "tick",
+          ticks_per_second: TICKS_PER_SECOND,
+        });
+      }, TICK_RATE);
+      break;
   }
 };
